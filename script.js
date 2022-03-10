545098e10a39b5eb7c8d7357b6dc6c5d6efa84f3
Emiliano Acerbi <eacerbip@gmail.com>
2022-03-09 21:14:45 -0300

    finishing details

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const accs = document.querySelectorAll('.header__nav__ul__item__acc')
 const mobileUlItems = document.querySelectorAll('.mobile__nav__ul__item__title')
 const mobileAccs = document.querySelectorAll('.mobile__nav__ul__item__acc')
 
+const hamb = document.querySelector('.header__nav__hamburguer')
+const mobileNav = document.querySelector('.mobile__nav')
+
 
 // Wide resolution 
 for (let item of ulItems) {
@@ -16,15 +19,15 @@ for (let item of ulItems) {
             }
 
             for (let acc of accs) {
-                acc.style.opacity = '0'
+                acc.style.maxHeight = '0'
             }
             
             item.classList.add('active')
-            item.nextElementSibling.style.opacity = '1'
+            item.nextElementSibling.style.maxHeight = item.nextElementSibling.scrollHeight + 'px'
             
         } else if (item.classList.contains('active')) {
             item.classList.remove('active')
-            item.nextElementSibling.style.opacity = '0'
+            item.nextElementSibling.style.maxHeight = '0'
         } 
     })
 }
@@ -52,6 +55,17 @@ for (let item of mobileUlItems) {
         } 
     })
 }
+
+hamb.addEventListener('click', () => {
+    console.log("test")
+    if (mobileNav.style.opacity === '1') {
+        mobileNav.style.opacity = '0'
+        mobileNav.style.top = '-50%'
+    } else {
+        mobileNav.style.opacity = '1'
+        mobileNav.style.top = '15%'
+    }
+})
 
 
 
